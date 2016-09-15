fe7cc3ea29c7cfa533f3d936bc8918913deae89b
Tom LeConey <tleconey@gmail.com>
2016-09-15 10:52:51 -0500

    added second, simpler solution and pseudo code document details

--- Chapter_2/Hash_Pyramid.js
+++ Chapter_2/Hash_Pyramid.js
@@ -1,10 +1,19 @@
+// Looping a Triangle
 
+// Make 7 calls to console.log logging n # where n is the line number
 
+// first solution
 var pyramid = "#";
+for (var x = 0; x < 7; x++) {
+  for (var y = 0; y <= x; y ++) {
+    if (pyramid.length <= x) {
+    pyramid += "#";
+    }
+  }
+  console.log(pyramid)
+}
 
-for (var x = 0; x < 8; x++) {
-	for (var y = 0; y <= x; y ++) {
-		pyramid += "#"
-	} 
-	console.log(pyramid + "\n")
+// simpler solution
+for (var pyramid = "#"; pyramid.length <= 7; pyramid += "#") {
+  console.log(pyramid)
 }
