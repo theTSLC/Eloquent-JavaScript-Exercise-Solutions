a364f0ca0d7d34f43c8787b8df739ccdef8828c8
Tom LeConey <tleconey@gmail.com>
2016-09-16 15:28:36 -0500

    solution code for Minimum

---
+++ Chapter_3/Minimum.js
@@ -0,0 +1,11 @@
+// Minimum
+
+// Write a function that when given two nonidentical numbers, return the smaller
+
+var minimum = function(number1, number2) {
+	if (number1 > number2) {
+		return number2;
+	} else {
+		return number1;
+	}
+}
