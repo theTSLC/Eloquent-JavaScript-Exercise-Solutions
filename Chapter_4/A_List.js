7af788007767084fb7da1493eb3e80aae8e13f9b
Tom LeConey <tleconey@gmail.com>
2016-09-19 19:55:40 -0500

    added function to convert list to array; 2 of 4 done

--- Chapter_4/A_List.js
+++ Chapter_4/A_List.js
@@ -2,18 +2,22 @@
 
 // Write out funtions associated with the List data structure.
 
-var arrayToList = function (array) {
+
+var arrayToList = function(array) {
 	var list = null;
-	for (var i = array.length-1; array >=0; i--) {
-    list = {value: array[i], rest: list};
+	for (var i = array.length-1; i >= 0; i--) {
+		list = {
+			value: array[i],
+			rest: list
+		};
 	}
 	return list;
 }
 
 var listToArray = function(list) {
 	var array = [];
-	for (var key in list) {
-	  array.push(list[key]);
-	  listToArray(list[key]);
+	for (var node = list; node; node = node.rest) {
+		array.push(node.value);
 	}
+	return array;
 }
