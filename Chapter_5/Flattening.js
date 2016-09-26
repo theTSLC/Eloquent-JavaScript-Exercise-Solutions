3144d5a92509892934171ea7e2bd3eaff6b4635a
Tom LeConey <tleconey@gmail.com>
2016-09-26 13:35:55 -0500

    Flattening solution complete

--- Chapter_5/Flattening.js
+++ Chapter_5/Flattening.js
@@ -1,7 +1,11 @@
 // Flattening
 
+// Write a function that uses reduce and concat to 'flaten' an array of arrays that still has all the emelents of the original array.
+
 var arrays = [[1,2,3,], [4,5], [6]];
 
-console.log(arrays.reduce(function(flat, current)){
-	return flat.concat(current)
-}, []);
+var flatten = function(arrays) {
+	return arrays.reduce(function(array, current) {
+    return array.concat(current)
+	}. [])
+}
