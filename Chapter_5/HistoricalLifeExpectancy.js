22c65a69c13922a5d34a2ef431b8cbfe37989df6
Tom LeConey <tleconey@gmail.com>
2016-09-27 20:59:27 -0500

    part 2 of historical life expectancy

--- Chapter_5/HistoricalLifeExpectancy.js
+++ Chapter_5/HistoricalLifeExpectancy.js
@@ -4,3 +4,15 @@ function average(array) {
   function plus(a, b) { return a + b; }
   return array.reduce(plus) / array.length;
 }
+
+function groupBy(array, groupOf) {
+  var groups = {};
+  array.forEach(function(element) {
+    var groupName = groupOf(element);
+    if (groupName in groups)
+      groups[groupName].push(element);
+    else
+      groups[groupName] = [element];
+  });
+  return groups;
+}
