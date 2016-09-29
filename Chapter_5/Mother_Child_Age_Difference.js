91b3024cb15f583fba5301d034a152e46edf5179
Tom LeConey <tleconey@gmail.com>
2016-09-28 23:27:37 -0500

    began Mother Child Age Difference

---
+++ Chapter_5/Mother_Child_Age_Difference.js
@@ -0,0 +1,10 @@
+function average(array) {
+  function plus(a, b) { return a + b; }
+  return array.reduce(plus) / array.length;
+}
+
+var byName = {};
+
+ancestry.forEach(function(person) {
+  byName[person.name] = person;
+});
